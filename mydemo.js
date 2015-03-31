120a68d89060562addf228c86d41642c20bef675
Fabien Benetou <fabien-services@benetou.fr>
2015-03-31 21:16:06 +0200

    First file with mostly goals

---
+++ mydemo.js
@@ -0,0 +1,21 @@
+console.log("http://vAtelier.net")
+
+// initiate a skybox that invites to think about your own notes, helps to focus and be creative
+console.log("http://learningthreejs.com/blog/2011/08/15/lets-do-a-sky/")
+
+// load a sub-graph of nodes with threshold of 10 nodes
+// if no differential positioning set default value of 0
+
+// load a plane at reasonnable distance from viewer
+
+// position nodes with force layout on the plane with differential positioning
+console.log("https://vida.io/documents/HRt7haiqq8Sbfh4nn")
+
+// apply differential positioning to nodes, change value with picking
+// handle the BT remote as picking with view
+console.log("http://soledadpenades.com/articles/three-js-tutorials/object-picking/")
+
+// interacting nodes and save potential differential positioning
+// save after a reasonnable pause after an interaction
+
+// apply ThreeJS stereo effect
