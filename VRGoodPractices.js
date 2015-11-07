be94782d0ed376368120791219514dcd27a94c86
Fabien Benetou <fabien-services@benetou.fr>
2015-11-07 11:29:46 +0100

    added a centralize file for VR good practices to reduce motion sickness

---
+++ VRGoodPractices.js
@@ -0,0 +1,3 @@
+console.log('Good Practices in VR to improve the experience');
+console.log('TODO: Add nose.');
+// http://www.purdue.edu/newsroom/releases/2015/Q1/virtual-nose-may-reduce-simulator-sickness-in-video-games.html
