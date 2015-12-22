add10fcb956110ec23e6ff3b95650f6f1b38259c
Fabien Benetou <fabien-services@benetou.fr>
2015-12-22 11:23:02 +0100

    Embedding highlighted code

--- VRLabWebsiteQuest/index.php
+++ VRLabWebsiteQuest/index.php
@@ -1,4 +1,9 @@
 <html>
+<head>
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.0.0/styles/default.min.css">
+<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.0.0/highlight.min.js"></script>
+<script>hljs.initHighlightingOnLoad();</script>
+</head>
 <style>
 body {
     top : 0;
@@ -9,7 +14,7 @@ body {
     padding : 0;
     overflow: hidden;
 }
-iframe {
+iframe#background {
     z-index : -9999;
     position: absolute;
     top : 0;
@@ -20,13 +25,30 @@ iframe {
     padding : 0;
     overflow: hidden;
 }
+iframe#inline {
+    z-index : -9998;
+    width   : 40%;
+    height  : 300px;
+    margin  : 40px;
+    padding : 0;
+    overflow: hidden;
+    opacity: 0.5;
+    float: left;
+}
+code {
+    float: right;
+    width   : 40%;
+    height  : 300px;
+    margin  : 40px;
+}
 #content {
-    opacity: 0.8;
+    height: 55%;
+    opacity: 0.9;
     background-color:white;
     color:black;
 }
 </style>
-<iframe src="./Background/"></iframe> 
+<iframe id="background" src="./Background/"></iframe> 
 <body>
 <center><img src="http://photos1.meetupstatic.com/photos/theme_head/b/5/c/0/full_6946528.jpeg"/></center>
 <br/>
@@ -39,10 +61,16 @@ iframe {
 - try the first workshop quest, either on your own or during our next workshop (<a href="http://www.meetup.com/VR-LAB-Brussels/events/227439481/">13th of January 2016</a>)
 -- broken webVR demo on half of the screen with 5 key steps to fix as a personnalized modifiable space (cf generate demo)
 - consider learning how to interact with some of the hardware we have
--- hardware item 1
--- hardware item 2
-
+-- hardware item 1 e.g. RevolVR http://revolvr.co
+-- hardware item 2 e.g. VicoVR http://vicovr.com
 </pre>
-<div>
+<div id="matching">
+<iframe id="inline" src="./Background/"></iframe>
+<pre>
+<code><?php readfile("./Background/edits.js");?>
+</code>
+</pre>
+</div>
+</div>
 </body>
 </html>
